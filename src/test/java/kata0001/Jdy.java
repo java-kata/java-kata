210fda803e6a98e823deb282557c486800adcbbb
devfuner <devfuner@pecina.co.kr>
2018-02-23 14:58:42 +0900

    first commit

---
+++ src/test/java/kata0001/Jdy.java
@@ -0,0 +1,14 @@
+package kata0001;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Jdy {
+
+    @Test
+    public void problem1() throws Exception {
+
+        assertEquals("", "");
+    }
+}
