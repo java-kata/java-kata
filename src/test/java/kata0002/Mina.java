6d2656b7dcd2b991eb06a71243c44b4748d5de00
mina1 <mina1@DESKTOP-CQNLRJ0>
2018-03-05 18:20:21 +0900

    kata0002 문제 업로드

---
+++ src/test/java/kata0002/Mina.java
@@ -0,0 +1,14 @@
+package kata0002;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Mina {
+
+    @Test
+    public void problem1() throws Exception {
+
+        assertEquals("", "");
+    }
+}
